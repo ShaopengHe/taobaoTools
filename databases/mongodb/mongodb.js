ee5b1ac53e5068555e8eb57005af93f54c4d7aa9
ShaopengHe <shaopenghe.she@gmail.com>
2012-12-20 01:06:06 +0800

    mongo find

--- databases/mongodb/mongodb.js
+++ databases/mongodb/mongodb.js
@@ -34,10 +34,47 @@ var MongoDB = function(options) {
     })
 };
 
+/**
+ * insert
+ * @param collection
+ * @param doc
+ * @param options
+ * @param cbf
+ */
 MongoDB.prototype.insert = function(collection, doc, options, cbf) {
     var args = Array.prototype.slice.call(arguments);
     var collection = this.db.collection(args.shift());
     collection.insert.apply(collection, args);
+};
+
+/**
+ * update
+ * @param collection
+ * @param query
+ * @param doc
+ * @param options -- {upsert: boolean, multi: boolean, safe:boolean}
+ * @param cbf
+ */
+MongoDB.prototype.update = function(collection, query, doc, options, cbf) {
+    var args = Array.prototype.slice.call(arguments);
+    var collection = this.db.collection(args.shift());
+    collection.update.apply(collection, args);
+};
+
+/**
+ * find
+ * @param collection
+ * @param query
+ * @param fields
+ * @param options
+ * @param cbf
+ */
+MongoDB.prototype.find = function(collection, query, fields, options, cbf) {
+    var args = Array.prototype.slice.call(arguments);
+    var collection = this.db.collection(args.shift());
+    cbf = args.pop();
+    var cursor = collection.find.apply(collection, args).toArray(cbf);
+    
 };
 
 module.exports = MongoDB;
@@ -46,15 +83,29 @@ var client = new MongoDB();
 setTimeout(
     function() {
 //        client.insert('test', {a:1});
-        var start = Date.now();
-        client.insert('test', {b:2}, function(err, docs) {
-            if (err) console.log(err);
-            console.log(docs);
-        });
+//        var start = Date.now();
+//        client.insert('test', {a:2, b:Math.random()}, function(err, docs) {
+//            if (err) console.log(err);
+//            console.log(Date.now() - start);
+//            console.log(docs);
+//        });
+//        var start = Date.now()
+//        client.update('test', {a:'update'}, {$set:{b:Math.random()}},{upsert:true}, function(err, docs) {
+//            if (err) console.log(err);
+//            console.log(Date.now() - start);
+//            console.log(docs);
+//        });
+//        var start = Date.now()
+//        client.update('test', {a:'update1'}, {$set:{b:Math.random()}},{upsert:true,safe:true}, function(err, docs) {
+//            if (err) console.log(err);
+//            console.log(Date.now() - start);
+//            console.log(docs);
+//        });
         var start = Date.now()
-        client.insert('test', {b:2}, {safe:true}, function(err, docs) {
+        client.find('test', {a:{$exists:true}},{_id:0},{skip:0,limit:20,sort:{b:1}}, function(err, doc){
             if (err) console.log(err);
-            console.log(docs);
+            console.log(Date.now() - start);
+            console.log(doc);
         });
     }, 1000
 );
